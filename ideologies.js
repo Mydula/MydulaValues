779fe4eb9f34a2f212a9fa1611a73abdcf9e0e7e
rusvalues <105062602+Mydula@users.noreply.github.com>
2022-10-04 20:56:31 +0300

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -211,7 +211,7 @@ ideologies = [
         "stats": {
             "econ": 80,
             "dipl": 0,
-            "govt": -10,
+            "govt": -15,
             "scty": 50
         } 
     },
