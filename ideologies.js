6f5b5ef400ce9cedb9ffea9490c3deee374e4c71
rusvalues <105062602+Mydula@users.noreply.github.com>
2023-09-17 17:03:51 +0300

    Add files via upload

--- ideologies.js
+++ ideologies.js
@@ -5,11 +5,11 @@ ideologies = [
             "fdrl": 50,
             "dmcr": 50,
             "govt": 50,
-            "nati": 50
+            "nati": 50,
             "milt": 50,
             "clnt": 50,
             "econ": 50,
-            "regl": 50
+            "regl": 50,
             "isol": 50,
             "secl": 50,
             "prog": 50,
